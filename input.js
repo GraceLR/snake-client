462c39e8b876b5cd675c7cf64516c44d1b506d92
GraceLR <gracerwork08@gmail.com>
2021-11-23 04:30:03 +0000

    conn

---
+++ input.js
@@ -0,0 +1,26 @@
+
+let connection;
+
+const handleUserInput = (key) => {
+    if(key === 'w') {connection.write("Move: up")};
+    if(key === 'a') {connection.write("Move: left")};
+    if(key === 's') {connection.write("Move: down")};
+    if(key === 'd') {connection.write("Move: right")};
+    if (key === '\u0003') {
+        process.exit();
+      }
+  }
+
+const setupInput = (conn) => {
+    connection = conn;
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+
+module.exports = {
+    setupInput
+  };
