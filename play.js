870ad489ce6944275069e2ceb52f3c8115e35535
GraceLR <gracerwork08@gmail.com>
2021-11-23 03:07:01 +0000

    stdin

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
 
 const {connect} = require("./client");
 
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    const handleUserInput = (key) => {
+        process.stdout.write('.');
+        if (key === '\u0003') {
+            process.exit();
+          }
+      }
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+
 console.log("Connecting ...");
 connect();
